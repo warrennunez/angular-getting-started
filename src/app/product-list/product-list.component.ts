271455446b9b1c0f7dc701a7970db791b8f6d063
warrennunez <warrennunez@gmail.com>
2019-12-15 08:17:26 +0800

    4 files modified, 2 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -22,6 +22,12 @@ export class ProductListComponent {
 
   share() {
     window.alert('The product has been shared!');
+    this.viewLog(products);
+  }
+
+  onNotify() {
+    this.viewLog(products);
+    window.alert('You will be notified when the product goes on sale');
   }
 }
 
